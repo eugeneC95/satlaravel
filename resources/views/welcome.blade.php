0c13b5462c2d8c3b94d2b5430f8ed371139e4cd5
zun95 <ilvinchan95@hotmail.com>
2019-05-21 17:54:50 +0900

    rewrite the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,70 +3,28 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>SNS v2.0</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+    <body class="bg-light">
+        <header class="col-md-4 my-5 mx-auto">
+            <h1 class="py-5  logo">
+
+                    <img class="col my-5 mx-auto" src="{{URL::asset('/image/logo.png')}}" alt="Logo" height="130" width="520">
+
+            </h1>
+        </header>
+        <nav class="navbar navbar-expand-lg bg-white text-uppercase">
+            <div class="container">
+                <a href="#logo">SNS</a>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item"><a class="nav-link" href="#intro">Introduce</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#skill">Skill</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#aboutme">AboutMe</a></li>
+                </ul>
+                @if (Route::has('login'))
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -76,24 +34,21 @@
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @endif
             </div>
-        </div>
+        </nav>
+        <section class="mx-5 text-center" id="intro">
+            <div class="px-5">
+                <p class="m-5 p-5">このプロジェクトは東京デザインテクノロジーセンター専門学校に授業のプロジェクトです。このプロジェクトはおそよ２ヶ月をかけて完成しました。</p>
+            </div>
+        </section>
+        <section class="" id="skills">
+            <p>開発においての困難</p>
+            HTML,CSS(bootstrap),Framework(Laravel5)
+        </section>
+        <section class="" id="aboutme">
+            A student from Malaysia in Japan.
+            Name: CHAN YOU CHUN
+        </section>
     </body>
 </html>
