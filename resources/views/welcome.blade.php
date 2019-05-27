457f20418da54ae5f06301848c4e95ee68b190c0
zun95 <ilvinchan95@hotmail.com>
2019-05-27 16:08:08 +0900

    made some changes on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,48 +7,80 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <style>
+            a{
+                color: #1a6ab0;
+            }
+            a:hover{
+                color: grey!important;
+                text-decoration: none;
+            }
+            footer{
+                background-color: #333;
+            }
+        </style>
     </head>
     <body class="bg-light">
         <header class="col-md-4 my-5 mx-auto">
-            <h1 class="py-5  logo">
-
-                    <img class="col my-5 mx-auto" src="{{URL::asset('/image/logo.png')}}" alt="Logo" height="130" width="520">
-
+            <h1 class="py-5 " id="logo">
+                <img class="col my-5 mx-auto" src="{{URL::asset('/image/logo.png')}}" alt="Logo" height="130" width="520">
             </h1>
         </header>
-        <nav class="navbar navbar-expand-lg bg-white text-uppercase">
-            <div class="container">
-                <a href="#logo">SNS</a>
-                <ul class="nav justify-content-end">
-                    <li class="nav-item"><a class="nav-link" href="#intro">Introduce</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#skill">Skill</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#aboutme">AboutMe</a></li>
-                </ul>
-                @if (Route::has('login'))
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                @endif
+        <nav class="navbar navbar-expand-lg bg-white text-uppercase shadow-sm">
+            <div class="container px-0">
+                <div><a href="#logo">SNS</a></div>
+                <div>
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item"><a class="nav-link" href="#intro">プロジェクト紹介</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#develop">開発</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#theme">テーマ</a></li>
+                    </ul>
+                </div>
+                <div>
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ url('/home') }}">ホーム</a>
+                        @else
+                            <a href="{{ route('login') }}">ログイン</a>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">登録</a>
+                            @endif
+                        @endauth
+                    @endif
+                </div>
             </div>
         </nav>
-        <section class="mx-5 text-center" id="intro">
+        <section class="mx-auto text-center" id="intro">
             <div class="p-5 col">
-                <p class="m-5 p-5">このプロジェクトは東京デザインテクノロジーセンター専門学校に授業のプロジェクトです。このプロジェクトはおそよ２ヶ月で完成する予定になっております。</p>
+                <h2 class="py-5 mt-5">プロジェクト紹介</h2>
+                <p class="p-5">このプロジェクトは東京デザインテクノロジーセンター専門学校に授業のプロジェクトです。</br>掲示板、SNSを真似して作り上げたサイトです。</p>
             </div>
         </section>
-        <section class="" id="skills">
-            <p>開発においての困難</p>
-            HTML,CSS(bootstrap),Framework(Laravel5)
+        <section class="mx-5 text-center" id="develop">
+            <div class="p-5 my-5">
+                <h2 class="p-5">開発</h2>
+                <p class="pt-5">開発言語</p>
+                <p>Framework(Laravel5),HTML,Bootstrap</p>
+            </div>
         </section>
-        <section class="" id="aboutme">
-            A student from Malaysia in Japan.
-            Name: CHAN YOU CHUN
+        <section class="mx-5 text-center" id="theme">
+            <div class="p-5 my-5">
+                <h2 class="p-5 my-5">テーマ</h2>
+                <p>SNSをベース、真似して作られた物です。</p>
+            </div>
         </section>
+        <section class="mx-5 text-center" id="contact">
+            <div class="p-5 my-5">
+                <h2 class="p-5 my-5">お問い合わせ</h2>
+                <p><a href="https://github.com/zun95/" target="blank"><img src="{{URL::asset('/image/github-logo.ico')}}" width="50" height="50"></a></p>
+                <p>Github</p>
+            </div>
+        </section>
+
+        <footer>
+            <div class="text-white-50 text-center pt-5 pb-3">
+                <p>Copyright©Eugene All Rights Reserved.</p>
+            </div>
+        </footer>
     </body>
 </html>
